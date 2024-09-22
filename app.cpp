b459ef172e0fe2e4fd8def0bac7b08d7078ada75
Datrooo <da231105@gmail.com>
2024-09-22 23:55:39 +0700

    Revert "lab0: first try code"
    This reverts commit 0d0ad04b730862c0896d8ab42cc739c4d87f366e.

--- app.cpp
+++
@@ -1,6 +0,0 @@
-#include "app.h"
-
-void App::process(std::ifstream& fin, std::ofstream& fout){
-    readerNcounter.readNcountFrequency(fin);
-    writer.writeCSV(fout, readerNcounter.getFrequency(), readerNcounter.getCntWords());
-}
