27ed898c9e0534061a428c24e831639e5d22a563
Datrooo <da231105@gmail.com>
2024-09-22 23:09:51 +0700

    lab0: загрузил код

---
+++ build/CMakeFiles/word_count.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/drakowkq/work/OOP/app.cpp" "CMakeFiles/word_count.dir/app.cpp.o" "gcc" "CMakeFiles/word_count.dir/app.cpp.o.d"
+  "/home/drakowkq/work/OOP/main.cpp" "CMakeFiles/word_count.dir/main.cpp.o" "gcc" "CMakeFiles/word_count.dir/main.cpp.o.d"
+  "/home/drakowkq/work/OOP/readNcount.cpp" "CMakeFiles/word_count.dir/readNcount.cpp.o" "gcc" "CMakeFiles/word_count.dir/readNcount.cpp.o.d"
+  "/home/drakowkq/work/OOP/writer.cpp" "CMakeFiles/word_count.dir/writer.cpp.o" "gcc" "CMakeFiles/word_count.dir/writer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
