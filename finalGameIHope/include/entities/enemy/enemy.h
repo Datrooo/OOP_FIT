6a29246a96856fe68b2bb1f077276c6b52ce1aae
Datrooo <da231105@gmail.com>
2024-12-08 18:16:57 +0700

    first try

---
+++ finalGameIHope/include/entities/enemy/enemy.h
@@ -0,0 +1,51 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "baseenemy.h"
+#include <QObject>
+#include <QTimer>
+#include "include/entities/direction.h"
+
+class Enemy : public QObject, public BaseEnemy {
+    Q_OBJECT
+public:
+    Enemy();
+    ~Enemy() override;
+
+    void takeDamage(int damage) override;
+    EntityType getEntityType() const override;
+
+    void startAttacking() override;
+    void stopAttacking() override;
+
+
+signals:
+    void enemyDead();
+    void secondStage();
+
+private slots:
+    void performAttack();
+    void changeToSecondStage();
+
+private:
+    char stage;
+    QTimer *attackTimer;
+    qreal healthPoint;
+    Direction direction;
+
+    QPixmap defaultPixmap;
+    QPixmap defaultReversePixmap;
+
+    void shootFireball();
+    void shootLaser();
+    void shootSpreadShot();
+    void advance(int phase) override;
+
+
+    qreal xMin;
+    qreal xMax;
+    qreal yMin;
+    qreal yMax;
+};
+
+#endif // ENEMY_H
